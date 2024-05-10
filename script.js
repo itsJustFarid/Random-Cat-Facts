2a7153217bb4943346be36b56e69fa8fc9107082
ItsJustFarid <107649013+itsJustFarid@users.noreply.github.com>
2024-05-10 19:59:03 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const cataasURL = "https://cataas.com/cat";
 const meowFactsURL = "https://meowfacts.herokuapp.com/";
 
+// Dialog
+
+function dialogTemplateHTML(dialogTitle, dialogMessage) {
+  return `<div class="dialog-overlay">
+  <div class="dialog">
+  <h1>${dialogTitle}</h1>
+    <p>${dialogMessage}</p>
+    <button class="btn-primary" id="dialogOk">OK</button>
+  </div>
+</div>`;
+}
+
+function showDialog(t, m) {
+  $(".container").prepend(dialogTemplateHTML(t, m));
+  $("#dialogOk").click(function () {
+    hideLoading();
+    $("#dialogOk")
+      .parents(".dialog-overlay")
+      .fadeOut(function () {
+        $("#dialogOk").parents(".dialog-overlay").remove();
+      });
+  });
+}
+
 // Load background
 
 $("main .loader").hide(); // This will hiding loading anim
@@ -20,19 +44,43 @@ $(".bg")
 // Fetching random facts
 
 function getFacts(count = 1, lang = "eng") {
+  return fetch(`${meowFactsURL}?${count}&${lang}`)
+    .then((Response) => {
+      if (!Response.ok) {
+        throw new Error(Response);
+      }
+      return Response.json();
+    })
+    .catch((error) => {
+      throw error;
+    });
+}
+
+function updateFact(data) {
+  $("#fact").html(data.data).css("font-size", "1em");
+  hideLoading();
+}
+
+$("#getFacts").click(async function () {
+  showLoading();
+  try {
+    const fact = await getFacts();
+    updateFact(fact);
+  } catch (err) {
+    showDialog(err.name, err.message);
+  }
+});
+
+// Display or hide the loader  when trying to fetching the url
+
+function showLoading() {
   $("#getFacts").attr("disabled", true);
   $(".instruction").fadeOut("fast", function () {
     $("main .loader").fadeIn("fast");
   });
-  fetch(`${meowFactsURL}?${count}&${lang}`)
-    .then((Response) => Response.json())
-    .then((Response) => {
-      $("#fact").html(Response.data).css("font-size", "1em");
-      $("main .loader").fadeOut("fast");
-      $("#getFacts").attr("disabled", false);
-    });
 }
 
-$("#getFacts").click(function () {
-  getFacts();
-});
+function hideLoading() {
+  $("main .loader").fadeOut("fast");
+  $("#getFacts").attr("disabled", false);
+}
